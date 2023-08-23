1047babca155dc7df47924e23bfd2e539b9a43a6
ARvaishnavi <141898980+ARvaishnavi@users.noreply.github.com>
2023-08-23 09:18:56 +0530

    vaishnavi.java
    welcome

---
+++ vaishnavi.java
@@ -0,0 +1,11 @@
+<html>
+<body>
+<p id="demo"></p>
+<script>
+let firstName=`Ehan`;
+let lastName=`h`;
+let text=`welcome ${firstName},${lastName}!`;
+document.getElementById("demo").innerHTML=text;
+</script>
+</body>
+</html>
